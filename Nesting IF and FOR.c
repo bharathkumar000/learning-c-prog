d64b7734aeb2323afbf4cf13a8b8ac1c21d6d302
bharathkumar000 <bharathece2006@gmail.com>
2026-08-05 12:42:02 +0530

    4

---
+++ Nesting IF and FOR.c
@@ -0,0 +1,33 @@
+/*branch inside a loop, how many big cities?
+
+You want to determine the number of cities in a given region that have a
+population strictly greater than 10,000. To do this, you write a program that
+first reads the number of cities in a region as an integer, and then the
+populations for each city one by one (also integers).
+
+Example
+Input
+6
+1000
+5000
+15000
+4780
+0
+23590
+Output
+2*/
+
+#include <stdio.h>
+int main() {
+  int i, n, m;
+  int num = 0;
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    scanf("%d", &m);
+    if (m > 10000) {
+      num = num + 1;
+    }
+  }
+  printf("%d", num);
+  return 0;
+}
