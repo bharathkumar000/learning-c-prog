40cf4945846a300faabda522fabb9306c5a8b8fb
bharathkumar000 <bharathece2006@gmail.com>
2026-08-05 12:43:07 +0530

    5

---
+++ Repeating inside repetition.c
@@ -0,0 +1,40 @@
+/*
+print a square of stars using nested loops
+
+Create a program that displays on the screen a square of n x n stars, with the integer n given as an input.
+
+Examples
+Input:
+5
+Output:
+*****
+*****
+*****
+*****
+*****
+ 
+Input:
+3
+Output:
+***
+***
+***
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+       
+    }
+    return 0;
+}
