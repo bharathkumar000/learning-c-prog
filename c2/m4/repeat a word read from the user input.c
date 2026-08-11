06b7ad9e05a5cbd11602cdfc6ecc95c7eca9e88b
bharathkumar000 <bharathece2006@gmail.com>
2026-08-11 15:59:26 +0530

    9

---
+++ c2/m4/repeat a word read from the user input.c
@@ -0,0 +1,32 @@
+/*Write a C-program that prints out a word as many times as specified. The number of repetitions and the word should be given as input to the program. You may assume that the word has no more than 100 characters (be sure to also reserve space for the null terminator, \0, though!).
+
+ 
+
+Examples
+Input:
+2 Hello
+Output:
+Hello
+Hello
+ 
+
+Input:
+4 thing
+Output:
+thing
+thing
+thing
+thing*/
+
+#include<stdio.h>
+int main()
+{
+int n , i;
+char word[101];
+scanf("%d %s",&n,word);
+for(i=0;i<n;i++)
+{
+    printf("%s\n",word);
+}
+return 0;
+}
